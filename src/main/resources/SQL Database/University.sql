f39ffdaa50a3e9ea02182aa5f306f5a89052fb18
HUHUITAO2003 <73886577+HUHUITAO2003@users.noreply.github.com>
2025-05-24 01:54:33 +0200

    Update University.sql

--- src/main/resources/SQL Database/University.sql
+++ src/main/resources/SQL Database/University.sql
@@ -1,25 +1,25 @@
-CREATE TABLE IF NOT EXISTS public.library_admin
+CREATE TABLE IF NOT EXISTS Library_admin
 (
-    user_code character varying(32),
-    name character varying(32),
-    surname character varying(32),
-    email character varying(128),
-    telephone_number character varying(32),
-    working_place character varying(128),
-    salt character varying(32),
-    hashed_password character varying(64),
-    CONSTRAINT library_admin_pkey PRIMARY KEY (user_code),
-    )
+    user_code VARCHAR(32),
+    name VARCHAR(32),
+    surname VARCHAR(32),
+    email VARCHAR(128),
+    telephone_number VARCHAR(32),
+    working_place VARCHAR(128),
+    salt VARCHAR(32),
+    hashed_password VARCHAR(64),
+    PRIMARY KEY (user_code)
+);
 
 
-CREATE TABLE IF NOT EXISTS public.university_people
+CREATE TABLE IF NOT EXISTS University_people
 (
-    user_code character varying(32),
-    name character varying(32),
-    surname character varying(32),
-    email character varying(128),
-    telephone_number character varying(32),
-    salt character varying(32),
-    hashed_password character varying(64),
-    CONSTRAINT university_people_pkey PRIMARY KEY (user_code),
-    )
+user_code VARCHAR(32),
+name VARCHAR(32),
+surname VARCHAR(32),
+email VARCHAR(128),
+telephone_number VARCHAR(32),
+salt VARCHAR(32),
+hashed_password VARCHAR(64),
+PRIMARY KEY (user_code)
+);
